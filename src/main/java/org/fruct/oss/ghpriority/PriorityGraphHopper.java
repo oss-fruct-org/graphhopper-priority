8aa01600c433524f35d4eab13c19a6696cdc06a5
Kirill Kulakov <kulakov@cs.karelia.ru>
2016-08-19 12:52:07 +0300

    update graphhopper version

--- src/main/java/org/fruct/oss/ghpriority/PriorityGraphHopper.java
+++ src/main/java/org/fruct/oss/ghpriority/PriorityGraphHopper.java
@@ -3,11 +3,11 @@ package org.fruct.oss.ghpriority;
 import com.graphhopper.GraphHopper;
 import com.graphhopper.routing.util.FlagEncoder;
 import com.graphhopper.routing.util.Weighting;
-import com.graphhopper.routing.util.WeightingMap;
+import com.graphhopper.routing.util.HintsMap;
 
 public class PriorityGraphHopper extends GraphHopper {
     @Override
-    public Weighting createWeighting(WeightingMap wMap, FlagEncoder encoder) {
+    public Weighting createWeighting(HintsMap wMap, FlagEncoder encoder) {
         if (encoder instanceof FootPriorityFlagEncoder) {
             return new FootPriorityWeighting(encoder);
         }
